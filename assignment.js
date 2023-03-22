2a58b2445f0bb521fee99d7991a536e5d512b548
nafea007 <killerkat36@gmail.com>
2023-03-22 23:09:43 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,25 @@
+
+// Create a JavaScript function called "multiplicationTable" that takes a single argument "num" (an integer) as input.
+
+
+// Inside the function, create a for loop that iterates from 1 to 10.
+
+
+// Inside the loop, multiply the current iteration number by the input "num" to generate the product.
+
+
+// Log the product to the console in the format "num x iteration = product".
+
+
+// Test the function with different input numbers to generate multiplication tables.
+
+
+  function multiplicationTable(num) {
+    for (var i = 1; i <= 10; i++) {
+      var product = num * i;
+      console.log(num + " x " + i + " = " + product);
+    }
+  }
+  multiplicationTable(3)
+  multiplicationTable(8)
+  multiplicationTable(12)
